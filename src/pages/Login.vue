2c1006943b61b2b3691897af0d40f6da997fdcf7
Jichani <123962013+jichani@users.noreply.github.com>
2025-03-09 23:26:02 +0900

    10. jwt 만들고 쿠키 저장까지 완료

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -61,6 +61,7 @@ export default {
         .post("/api/account/login", state.form)
         .then((res) => {
           store.commit("setAccount", res.data);
+          console.log(res.data);
           sessionStorage.setItem("id", res.data);
           router.push({ path: "/" });
           window.alert("로그인하였습니다.");
